d1166b3980e9d9bfbf5d1d6cb3cc0950800deb70
Iswanson81459 <Iswanson81459@uat.edu>
2025-11-13 15:14:58 -0700

    First Push
    Has First Models. Start of Pawn Scheme.

---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float yawSpeed;
+    public float pitchSpeed;
+    public float rollSpeed;
+
+    public float booster;
+
+
+    //[Header("Components")]
+    
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveForward()
+    {
+
+    }
+
+    void MoveBackward()
+    {
+
+    }
+
+    void Turn()
+    {
+
+    }
+
+    void Roll()
+    {
+
+    }
+
+    void Pitch()
+    {
+
+    }
+
+}
